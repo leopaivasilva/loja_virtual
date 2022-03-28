2295cd633164f5f300407fbd6ecf99a62c6f9fd6
leopa <leopa@LeoNoteAvell>
2022-03-28 13:53:09 -0300

    Criando a classe Categoria Produto e alterado o campo de descrição da classe marca produto

--- src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -1,6 +1,7 @@
 package jdev.mentoria.lojavirtual.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,14 +12,14 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
 
 	@Column(nullable = false)
@@ -38,6 +39,23 @@ public class MarcaProduto implements Serializable {
 
 	public void setNomeDesc(String nomeDesc) {
 		this.nomeDesc = nomeDesc;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaProduto other = (CategoriaProduto) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
