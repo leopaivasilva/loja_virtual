f163435eeee82b7d1e47c1f4e345d7c1bbafc1d2
leopa <leopa@LeoNoteAvell>
2022-03-29 11:45:21 -0300

    Criado Classe Produto, faltando implementar nota item

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/Produto.java
@@ -0,0 +1,195 @@
+package jdev.mentoria.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+@SequenceGenerator(name = "seq_produto", sequenceName = "seq_produto", allocationSize = 1, initialValue = 1)
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
+	private Long id;
+
+	private String tipoUnidade;
+	
+	private String nome;
+	
+	@Column(columnDefinition = "text", length = 2000)
+	private String descricao;
+	
+	/*nota item produto*/
+	
+	private Double peso;
+	
+	private Double largura;
+	
+	private Double altura;
+	
+	private Double profundidade;
+	
+	private BigDecimal valorVenda = BigDecimal.ZERO;
+	
+	private Integer QtdeEstoque = 0;
+	
+	private Integer QtdeAlertaEstoque = 0;
+	
+	private String linkYouTube;
+	
+	private Boolean alertaQtdeEstoque = Boolean.FALSE;
+	
+	private Boolean ativo = Boolean.TRUE;
+	
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	private Integer qtdeClique = 0;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipoUnidade() {
+		return tipoUnidade;
+	}
+
+	public void setTipoUnidade(String tipoUnidade) {
+		this.tipoUnidade = tipoUnidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Double getLargura() {
+		return largura;
+	}
+
+	public void setLargura(Double largura) {
+		this.largura = largura;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getProfundidade() {
+		return profundidade;
+	}
+
+	public void setProfundidade(Double profundidade) {
+		this.profundidade = profundidade;
+	}
+
+	public BigDecimal getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(BigDecimal valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public Integer getQtdeEstoque() {
+		return QtdeEstoque;
+	}
+
+	public void setQtdeEstoque(Integer qtdeEstoque) {
+		QtdeEstoque = qtdeEstoque;
+	}
+
+	public Integer getQtdeAlertaEstoque() {
+		return QtdeAlertaEstoque;
+	}
+
+	public void setQtdeAlertaEstoque(Integer qtdeAlertaEstoque) {
+		QtdeAlertaEstoque = qtdeAlertaEstoque;
+	}
+
+	public String getLinkYouTube() {
+		return linkYouTube;
+	}
+
+	public void setLinkYouTube(String linkYouTube) {
+		this.linkYouTube = linkYouTube;
+	}
+
+	public Boolean getAlertaQtdeEstoque() {
+		return alertaQtdeEstoque;
+	}
+
+	public void setAlertaQtdeEstoque(Boolean alertaQtdeEstoque) {
+		this.alertaQtdeEstoque = alertaQtdeEstoque;
+	}
+
+	public Integer getQtdeClique() {
+		return qtdeClique;
+	}
+
+	public void setQtdeClique(Integer qtdeClique) {
+		this.qtdeClique = qtdeClique;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
