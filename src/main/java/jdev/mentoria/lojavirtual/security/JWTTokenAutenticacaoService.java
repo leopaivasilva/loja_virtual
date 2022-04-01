ce1839800bb3c9c21e121fa40676a47ad00849d7
leopa <leopa@192.168.0.224>
2022-04-01 13:51:52 -0300

    Implementando Erro de Cors

--- src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -41,9 +41,33 @@ public class JWTTokenAutenticacaoService {
 		/*Dar a resposta para tela e para o cliente, pode ser em outra API, navegador, aplicativo, javascript, chamada java*/
 		response.addHeader(HEADER_STRING, token);
 		
+		liberacaoCors(response);
+		
 		/*Usado para ver no postman para teste*/
 		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+	
+	}
+	
+	/*Fazendo liberação contra erro de Cors no navegador*/
+	private void liberacaoCors(HttpServletResponse response) {
 		
-	}          
+		if (response.getHeader("Access-Control-Allow-Origin") == null) {
+			response.addHeader("Access-Control-Allow-Origin", "*");				
+		}
+		
+		if (response.getHeader("Access-Control-Allow-Headers") == null) {
+			response.addHeader("Access-Control-Allow-Headers", "*");				
+		}
+		
+		if (response.getHeader("Access-Control-Request-Headers") == null) {
+			response.addHeader("Access-Control-Request-Headers", "*");				
+		}
+		
+		if (response.getHeader("Access-Control-Allow-Methods") == null) {
+			response.addHeader("Access-Control-Allow-Methods", "*");				
+		}		
+		
+	}
+	
 
 }
