a0f2feaa310862ad52c3d5a34905581aea25a923
leopa <leopa@192.168.0.12>
2022-04-12 20:26:31 -0300

    Consumindo API de CEP

--- src/main/java/jdev/mentoria/lojavirtual/service/PessoaUserService.java
+++ src/main/java/jdev/mentoria/lojavirtual/service/PessoaUserService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import jdev.mentoria.lojavirtual.model.PessoaFisica;
 import jdev.mentoria.lojavirtual.model.PessoaJuridica;
 import jdev.mentoria.lojavirtual.model.Usuario;
+import jdev.mentoria.lojavirtual.model.dto.CepDTO;
 import jdev.mentoria.lojavirtual.repository.PessoaRepository;
 import jdev.mentoria.lojavirtual.repository.PesssoaFisicaRepository;
 import jdev.mentoria.lojavirtual.repository.UsuarioRepository;
@@ -85,6 +87,11 @@ public class PessoaUserService {
 		return pessoaJuridica;
 		
 	} 
+	
+	public CepDTO consultaCEP(String cep) {
+		return new RestTemplate().getForEntity("https://viacep.com.br/ws/" + cep + "/json", CepDTO.class).getBody();
+		
+	}
 	
 	
 	public PessoaFisica salvarPessoaFisica(PessoaFisica pessoaFisica) {
