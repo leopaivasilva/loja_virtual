b3aa43630a551671ff69a1f172b1e1a37e1dadc2
leopa <leopa@192.168.0.12>
2022-04-22 11:53:57 -0300

    Controle de End points para pessoa

--- src/main/java/jdev/mentoria/lojavirtual/repository/PessoaRepository.java
+++ src/main/java/jdev/mentoria/lojavirtual/repository/PessoaRepository.java
@@ -1,5 +1,7 @@
 package jdev.mentoria.lojavirtual.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,13 +12,29 @@ import jdev.mentoria.lojavirtual.model.PessoaJuridica;
 @Repository
 public interface PessoaRepository extends CrudRepository<PessoaJuridica, Long> {
 	
+	@Query(value = "select pj from PessoaJuridica pj where upper(trim(pj.nome)) like %?1%")
+	public List<PessoaJuridica> pesquisaPorNomePJ(String nome);
+	
 	@Query(value = "select pj from PessoaJuridica pj where pj.cnpj = ?1")
 	public PessoaJuridica existeCNPJCadastrado(String cnpj);
 	
+	@Query(value = "select pj from PessoaJuridica pj where pj.cnpj = ?1")
+	public List<PessoaJuridica> existeCNPJCadastradoList(String cnpj);
+	
+	
 	@Query(value = "select pf from PessoaFisica pf where pf.cpf = ?1")
-	public PessoaFisica existeCpfCadastrado(String cpf);	
+	public PessoaFisica existeCpfCadastrado(String cpf);
+	
+	
+	@Query(value = "select pf from PessoaFisica pf where pf.cpf = ?1")
+	public List<PessoaFisica> existeCpfCadastradoList(String cpf);
+	
 	
 	@Query(value = "select pj from PessoaJuridica pj where pj.inscEstadual = ?1")
-	public PessoaJuridica existeInscEstadualCadastrado(String inscEstadual);	
+	public PessoaJuridica existeInscEstadualCadastrado(String inscEstadual);
+	
+	
+	@Query(value = "select pj from PessoaJuridica pj where pj.inscEstadual = ?1")
+	public List<PessoaJuridica> existeInscEstadualCadastradoList(String inscEstadual);	
 
 }
--- src/main/java/jdev/mentoria/lojavirtual/repository/PesssoaFisicaRepository.java
+++ src/main/java/jdev/mentoria/lojavirtual/repository/PesssoaFisicaRepository.java
@@ -1,5 +1,9 @@
 package jdev.mentoria.lojavirtual.repository;
 
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +13,12 @@ import jdev.mentoria.lojavirtual.model.PessoaFisica;
 public interface PesssoaFisicaRepository extends CrudRepository<PessoaFisica, Long> {
 	
 	
+	@Query(value = "select pf from PessoaFisica pf where upper(trim(pf.nome)) like %?1%")
+	public List<PessoaFisica> pesquisaPorNomePF(String nome);
 	
+	
+	@Query(value = "select pf from PessoaFisica pf where pf.cpf = ?1")
+	public List<PessoaFisica> pesquisaPorCpfPF(String cpf);	
 	
 
 }
